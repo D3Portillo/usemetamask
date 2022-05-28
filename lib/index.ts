557637b0af3be20e5805ddd1bc40300b6a4b6037
D3Portillo <d3portillo@gmail.com>
2022-05-28 07:42:54 +0200

    chores(/lib/index.ts): add library implementation exports

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+export { default as useMetamask } from "./useMetamask"
+
+export {
+  addEtherNetwork,
+  addEtherToken,
+  getMetamaskProvider,
+  switchEtherNetwork,
+  switchOrAppendNetwork,
+  etherSend,
+  etherRequest,
+  parse,
+} from "./utils"
+
+export { default as NETWORK } from "./NETWORK"
