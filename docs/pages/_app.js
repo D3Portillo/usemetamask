da40972bc63f3f5c5502ee3ded3f382f94d74f9a
D3Portillo <d3portillo@gmail.com>
2022-06-13 13:49:28 +0200

    fix(_app.js): hide mobile menu onResize only if X width changes

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -5,12 +5,16 @@ import "../styles.css"
 const ON_RESIZE = "resize"
 export default function Nextra({ Component, pageProps }) {
   useEffect(() => {
+    let storedWidth = window.innerWidth
     function closeMobileMenu() {
+      const newWidth = window.innerWidth
       const navButton = document.querySelector("nav button.md\\:hidden")
+      const isResizeXEvent = storedWidth !== newWidth
       const isShowingMenu = document.body.classList.contains("overflow-hidden")
-      if (isShowingMenu && navButton) {
+      if (isShowingMenu && navButton && isResizeXEvent) {
         navButton.click()
       }
+      storedWidth = newWidth
     }
     window.addEventListener(ON_RESIZE, closeMobileMenu)
     return () => {
