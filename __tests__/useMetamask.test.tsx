e9f6ee4e1428efcb4c2b64e175ff00107aaf650a
D3Portillo <d3portillo@gmail.com>
2022-06-14 19:25:01 +0200

    chores: enhance test cases

--- __tests__/useMetamask.test.tsx
+++ __tests__/useMetamask.test.tsx
@@ -1,11 +1,17 @@
 import * as React from "react"
-import { render } from "@testing-library/react"
+import { act, render } from "@testing-library/react"
 
+import { UseMatamaskAPI } from "../lib/shared"
 import { useMetamask } from "../lib/index"
 
-function ExposeHook({ onRender = (_) => {}, onMetamask = undefined }) {
-  const result = useMetamask(onMetamask)
-  onRender(result)
+import { ACCOUNTS } from "../__utils__/constans"
+import { exposeMetamask, waitForUseEffect } from "../__utils__"
+
+function ExposeHook({
+  onRender = (_: UseMatamaskAPI) => {},
+  onMetamask = undefined,
+}) {
+  onRender(useMetamask(onMetamask))
   return null
 }
 
@@ -17,8 +23,9 @@ describe("useMetamask", () => {
     }
     jest.clearAllMocks()
   })
+
   it("should match empty state & no metamask", () => {
-    const state = {
+    const expectedState = {
       chainIdDecimal: 0,
       chainId: "0x0",
       metamask: {},
@@ -29,6 +36,41 @@ describe("useMetamask", () => {
     const onRender = jest.fn()
     node = render(<ExposeHook onRender={onRender} />)
     expect(node.container).toMatchSnapshot()
-    expect(onRender).toBeCalledWith(expect.objectContaining(state))
+    expect(onRender).toBeCalledWith(expect.objectContaining(expectedState))
+  })
+
+  it("should render account='0x111'", async () => {
+    const expectedState = {
+      account: ACCOUNTS[0],
+      accounts: ACCOUNTS,
+    }
+    exposeMetamask({
+      request: jest.fn().mockResolvedValue(ACCOUNTS),
+    })
+    const mockOnRender = jest.fn()
+    node = render(<ExposeHook onRender={mockOnRender} />)
+    await waitForUseEffect(node)
+    expect(mockOnRender).toBeCalledWith(expect.objectContaining(expectedState))
+  })
+
+  it("should disconnect when connected", async () => {
+    let api: UseMatamaskAPI = {} as any
+    const expectedState = {
+      isConnected: true,
+    }
+    exposeMetamask({
+      request: jest.fn().mockResolvedValue(ACCOUNTS),
+    })
+    const mockOnRender = jest.fn((_api) => {
+      api = _api
+    })
+    node = render(<ExposeHook onRender={mockOnRender} />)
+    await waitForUseEffect(node)
+    expect(mockOnRender).toBeCalledWith(expect.objectContaining(expectedState))
+    expect(api.isConnected).toBeTruthy()
+    act(() => {
+      api.disconnect()
+    })
+    expect(api.isConnected).toBeFalsy()
   })
 })
