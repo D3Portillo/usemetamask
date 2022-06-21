d7ad1e9db390169d184931882b88b2e1ed1598f4
D3Portillo <d3portillo@gmail.com>
2022-06-21 05:50:08 +0200

    enh(getFIATValue): move & rename getFIATPrice to it'separate file. Now function expects a currency string

---
+++ lib/utils/getFIATValue.ts
@@ -0,0 +1,21 @@
+const FORMATTERS = {
+  USD: new Intl.NumberFormat("USD"),
+  JPY: new Intl.NumberFormat("JPY"),
+  EUR: new Intl.NumberFormat("EUR"),
+}
+
+const defaultFormatter = {
+  format: (n) => {
+    console.error(
+      "getFIATBalance:: Error, `currency` expects oneOf('USD','JPY','EUR')"
+    )
+    return `${n}`
+  },
+}
+
+const getFIATValue = (tokenBalance, price, currency = "USD") => {
+  const formatter: Intl.NumberFormat = FORMATTERS[currency] || defaultFormatter
+  return formatter.format(tokenBalance * price)
+}
+
+export default getFIATValue
